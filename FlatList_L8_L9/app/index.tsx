2c0d76dd97ef1e4d58c9084c00ce991e920cd246
WolverArbor <126630674+WolverArbor@users.noreply.github.com>
2025-09-12 11:31:37 -0500

    9/12 notes

--- FlatList_L8_L9/app/index.tsx
+++ FlatList_L8_L9/app/index.tsx
@@ -1,14 +1,35 @@
-import {
-  Text,
-  View,
-  StyleSheet,
-  FlatList,
-  TouchableOpacity,
-} from "react-native";
 import colors from "@/styles/colors";
 import defaultStyles from "@/styles/defaultStyles";
+import { useState } from "react";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from "react-native";
 
 export default function Index() {
+  
+  // declare datatype for each item in array
+  type dataType = {
+    id: string; // unique identifier
+    title: string; // text displayed in flatlist
+  }
+  const DATA: dataType[] = [
+    {id: "1", title: "First Item"},
+    {id: "2", title: "Second Item"},
+    {id: "3", title: "Third Item"},
+    {id: "4", title: "Fourth Item"},
+  ]
+  
+  // create a state var to keep track of selected id
+  const [selectedId, setSelectedId] = useState<string>("1");
+
+  const itemSelected = (item: dataType) => {
+    console.log(item.title);
+    setSelectedId(item.id);
+  }
   return (
     <View style={defaultStyles.container}>
       <View style={defaultStyles.titleContainer}>
@@ -16,7 +37,21 @@ export default function Index() {
       </View>
       <View style={[defaultStyles.textContainer, { flex: 1 }]}>
         <View style={styles.flatlist}>
-          <Text>This is where our list will go</Text>
+          <FlatList
+            data = {DATA}
+            keyExtractor = {(item: dataType) => item.id}
+            renderItem ={({item}) => (
+              <TouchableOpacity onPress={() => itemSelected(item)}>
+                <View style={[styles.flatListRow, {
+                  backgroundColor: item.id === selectedId ? colors.primary : colors.secondary
+                }]}>
+                  <Text style={[{
+                    color: item.id === selectedId ? colors.text.light : colors.text.dark
+                  }]}>{item.title}</Text>
+                </View>
+              </TouchableOpacity>
+            )}
+          />
         </View>
       </View>
     </View>
@@ -26,6 +61,12 @@ export default function Index() {
 const styles = StyleSheet.create({
   flatlist: {
     alignItems: "center",
+  },
+  flatListRow: {
+    backgroundColor: "pink",
+    margin: 5,
+    padding: 5,
+    width: 100,
   },
   titleContainer: {
     marginTop: 5,
